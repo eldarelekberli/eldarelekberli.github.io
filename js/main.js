9b02c0fdd896c95243d39e306aeb15b19e094c4a
eldar <eldarelekberli@gmail.com>
2017-01-03 16:51:18 +0400

    ilk cehd

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function (){
-  var latlng = new google.maps.LatLng(41.386697,2.163923);
+  var latlng = new google.maps.LatLng(40.57441781053188, 49.77759513408512);
   var options = {
-    zoom: 15,
+    zoom: 13,
     center: latlng,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     navigationControl: true,
@@ -22,8 +22,8 @@ $(document).ready(function (){
 
 
   var infowindow = new google.maps.InfoWindow({
-    content:  '<div class="info"><strong>Universitat de Barcelona</strong><br />' 
-    + '<br/ >Gran Via de les Corts Catalanes, 585<br />08007 Barcelona<br> Building a room TBD</div>'
+    content:  '<div class="info"><strong>AF-HOTEL Aqua Park</strong><br />' 
+    + '<br/ ><br /> Baku<br> Azerbaijan</div>'
   });
 
   infowindow.open(map, marker1);
