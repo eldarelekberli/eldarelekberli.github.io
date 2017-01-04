29671163fd576558ed7008b132d7158316caaf60
eldar <eldarelekberli@gmail.com>
2017-01-04 19:51:17 +0400

    some fixex

--- js/main.js
+++ js/main.js
@@ -1,33 +1,9 @@
 $(document).ready(function (){
-  var latlng = new google.maps.LatLng(40.57441781053188, 49.77759513408512);
-  var options = {
-    zoom: 13,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    navigationControl: true,
-    mapTypeControl: false,
-    scrollwheel: false,
-    disableDoubleClickZoom: true
-  };
-
-  var map = new google.maps.Map(document.getElementById('map'), options);
-
-  var marker1 = new google.maps.Marker({
-    position: latlng, map: map
-  });
-
-  google.maps.event.addListener(marker1, 'click', function() {
-    infowindow.open(map, marker1);
-  });
-
-
-  var infowindow = new google.maps.InfoWindow({
-    content:  '<div class="info"><strong>AF-HOTEL Aqua Park</strong><br />' 
-    + '<br/ ><br /> Baku<br> Azerbaijan</div>'
-  });
-
-  infowindow.open(map, marker1);
   
+  new WOW().init();
+
+
+
   new WOW().init();
 
   var scrollIt = function(){    
@@ -44,6 +20,5 @@ $(document).ready(function (){
   $("#results").lazyload({
       effect : "fadeIn"
   });
-
 });
 
