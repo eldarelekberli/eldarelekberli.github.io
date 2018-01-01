6da8bc135d8d0a47794612f7b574c0f55636b418
eldar <eldarelekberli@gmail.com>
2018-01-01 20:56:01 +0400

    officially ready

---
+++ 2017/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function (){
+  
+  new WOW().init();
+
+  var scrollIt = function(){    
+    $('a[data-section]').click(function(e){
+        var section =  $(this).attr('data-section');
+        e.preventDefault();        
+        $('html, body').animate({
+            scrollTop: $('#'+section).offset().top-50 + "px"
+        }, 2000);
+    });
+  }
+  scrollIt();
+
+  $("#results").lazyload({
+      effect : "fadeIn"
+  });
+});
+
